322746530fbefaf95bda61ca0d98f488111a49fb
Aseuc <busse1997@googlemail.com>
2023-12-05 18:12:40 +0100

    Add image upload functionality

--- deploy/deploy.py
+++ deploy/deploy.py
@@ -1,3 +1,9 @@
 import streamlit as st
+from PIL import Image
 
+uploaded_file = st.file_uploader("Choose an image...", type="jpg")
 
+if uploaded_file is not None:
+    image = Image.open(uploaded_file)
+    st.image(image, caption='Uploaded Image.', use_column_width=True)
+    st.write("Image successfully uploaded.")
