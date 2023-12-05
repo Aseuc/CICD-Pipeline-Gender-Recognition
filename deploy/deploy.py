a11d3c75e124114483a18adcb7b77d34f9d0acaa
Aseuc <busse1997@googlemail.com>
2023-12-05 21:25:10 +0100

    Resize image transformation removed

--- deploy/deploy.py
+++ deploy/deploy.py
@@ -56,7 +56,7 @@ class SimpleCNN(nn.Module):
 def predict(image, model_path):
     # Define the transformation
     transform = transforms.Compose([
-        transforms.Resize((32, 32)),
+
         transforms.ToTensor(),
         transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
     ])
