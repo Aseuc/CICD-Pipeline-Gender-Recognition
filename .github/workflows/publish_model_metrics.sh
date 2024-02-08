b68ac21c3a921389b7c85149e9d44b3b57c9c722
Aseuc <busse1997@googlemail.com>
2024-02-09 00:49:23 +0100

    Fix publishing script and update report generation

--- .github/workflows/publish_model_metrics.sh
+++ .github/workflows/publish_model_metrics.sh
@@ -1,16 +1,16 @@
 publish_model_metrics() {
     echo "## Model Metriken" > report_ml.md
     cat test/metrics/metrics.txt >> report_ml.md
-    echo 'cml-publish   test/metrics/metrics.jpg --md >> report_ml.md' > publish_metrics.sh
-    cml-send-comment report_ml.md
+    cml-publish   "test/metrics/metrics.jpg" --md >> "report_ml.md" > "publish_metrics.sh"
+    cml-send-comment "report_ml.md"
 }
 
 
 publish_model_fairness_metrics(){
-    echo "\n## Fairlearn Ergebnisse" >> report_ml.md
-    echo 'cml-publish test/metricsFairlearn/Fig1metricsFairLearn.jpg --md >> report_ml.md' >> publish_metrics.sh
-    echo 'cml-publish test/metricsFairlearn/Fig2metricsFairLearn.jpg --md >> report_ml.md' >> publish_metrics.sh
-    cml-send-comment report_ml.md
+    echo "\n## Fairlearn Ergebnisse" >> "report_ml.md"
+    cml-publish "test/metricsFairlearn/Fig1metricsFairLearn.jpg" --md >> "report_ml.md" >> "publish_metrics.sh"
+    cml-publish "test/metricsFairlearn/Fig2metricsFairLearn.jpg" --md >> "report_ml.md" >> "publish_metrics.sh"
+    cml-send-comment "report_ml.md"
 }
 
 publish_model_metrics_noise(){
@@ -19,9 +19,9 @@ publish_model_metrics_noise(){
             echo "## Modellmetriken mit verauschten Bilder" > $report_file      
             for file in $noise_plot_data_dir/*.png; do
                 echo "Veröffentlichung $file"
-                cml-publish "$file" --md >> $report_file
+                cml-publish "$file" --md >> "$report_file"
             done
-            cml-send-comment $report_file
+            cml-send-comment "$report_file"
             
 }
 publish_model_metrics_disortion(){
@@ -32,19 +32,19 @@ publish_model_metrics_disortion(){
         echo "Veröffentlichung $file"
         cml-publish "$file" --md >> $report_file
     done
-    cml-send-comment $report_file
+    cml-send-comment "$report_file"
             
 }
 
 publish_model_metrics_rotation(){
     local report_file=$1
     local rotation_plot_data_dir=$2
-    echo "## Modellmetriken mit verdrehte Bilder" > $report_file      
+    echo "## Modellmetriken mit verdrehte Bilder" > "$report_file"      
     for file in $rotation_plot_data_dir/*.png; do
         echo "Publishing $file"
-        cml-publish "$file" --md >> $report_file
+        cml-publish "$file" --md >> "$report_file"
     done
-    cml-send-comment $report_file
+    cml-send-comment "$report_file"
 }
 
            
