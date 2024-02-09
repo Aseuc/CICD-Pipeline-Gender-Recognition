7532c37b96e6cbc130aa0ec1201259e2d811df90
Aseuc <busse1997@googlemail.com>
2024-02-09 03:46:47 +0100

    Update user authentication logic

--- .github/workflows/model_metrics.sh
+++ .github/workflows/model_metrics.sh
@@ -2,14 +2,14 @@ publish_model_metrics() {
     echo "## Model Metriken" > report_ml.md
     cat test/metrics/metrics.txt >> report_ml.md
     cml-publish "test/metrics/metrics.jpg" --md >> report_ml.md
-    cml-send-comment report_ml.md
+    # cml-send-comment report_ml.md
 }
 
 publish_model_fairness_metrics(){
     echo "\n## Fairlearn Ergebnisse" >> report_ml.md
     cml-publish "test/metricsFairlearn/Fig1metricsFairLearn.jpg" --md >> report_ml.md
     cml-publish "test/metricsFairlearn/Fig2metricsFairLearn.jpg" --md >> report_ml.md
-    cml-send-comment report_ml.md
+    # cml-send-comment report_ml.md
 }
 
 publish_model_metrics_noise(){
@@ -21,7 +21,7 @@ publish_model_metrics_noise(){
                     echo "Veröffentlichung $file"
                     cml-publish "$file" --md >> "$report_file"
                 done
-                cml-send-comment "$report_file"
+                # cml-send-comment "$report_file"
             fi
             
 }
@@ -34,7 +34,7 @@ publish_model_metrics_disortion(){
             echo "Veröffentlichung $file"
             cml-publish "$file" --md >> "$report_file"
         done
-        cml-send-comment "$report_file"
+        # cml-send-comment "$report_file"
     fi
             
 }
@@ -53,7 +53,7 @@ publish_model_metrics_rotation "report_ml.md" "test/test-plots-verdrehung"
 publish_model_metrics_rotation(){
     local report_file=$1
     local rotation_plot_data_dir=$2
-    echo "## Modellmetriken mit verdrehte Bilder" > "$report_file"      
+    echo "## Modellmetriken mit verdrehten Bilder" > "$report_file"      
     for file in $rotation_plot_data_dir/*.png; do
         echo "Publishing $file"
         cml-publish "$file" --md >> "$report_file"
@@ -89,3 +89,4 @@ publish_model_metrics_noise "report_ml.md" "test/test-plots-rauschen/"
 publish_model_metrics_disortion "report_ml.md" "test/test-plots-verzerrung/"
 publish_model_metrics_rotation "report_ml.md" "test/test-rotation/"
 publish_activation_map
+cml-send-comment "report_ml.md"
